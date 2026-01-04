bf810032bb36ce6d6d683e711daa202885f77d39
Adinath Ramchandra More <moreadi7777@gmail.com>
2026-01-04 19:59:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function showToast(){
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the Error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+function showToast(msg){
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = 'success';
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('Error')){
+    toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+    toast.classList.add('invalid');
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    }, 5200);
 }
